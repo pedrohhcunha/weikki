8f63564c90fa5c0b900c7efeb693a560e7dabd78
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-13 17:52:30 -0300

    Estilizando o header

--- components/Header/componente.jsx
+++ components/Header/componente.jsx
@@ -9,20 +9,20 @@ import Button from '../Button/componente.jsx'
 export default function Header(props){
     return(
         <header className={styles.header}>
-            <Image src={logoPrincipal} />
-            <div className="leftArea">
-                <nav>
-                    <ul>
-                        <li><Link href=""><a>A weikki</a></Link></li>
-                        <li><Link href=""><a>Soluções</a></Link></li>
-                        <li><Link href=""><a>Diferenciais</a></Link></li>
+            <div className={styles.areaImagem}></div>
+            <div className={styles.rightArea}>
+                <nav className={styles.nav}>
+                    <ul className={styles.ul}>
+                        <li className={styles.item}><Link href=""><a>A weikki</a></Link></li>
+                        <li className={styles.item}><Link href=""><a>Soluções</a></Link></li>
+                        <li className={styles.item}><Link href=""><a>Diferenciais</a></Link></li>
                     </ul>
                 </nav>
-                <div className="buttons">
+                <div className={styles.buttons}>
                     <Link href="">
                         <a className={styles.linkTelefone}>
-                            <FontAwesomeIcon icon={faPhone} />
-                            <span>49 33222593</span>
+                            <FontAwesomeIcon className={styles.icon} icon={faPhone} />
+                            <span className={styles.phoneNumber}>49 3322.2593</span>
                         </a>
                     </Link>
                     <Button large>Comprar agora</Button>
