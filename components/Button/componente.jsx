36bbe72846bbf3fd59a4124b1cdf1639572f5010
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-13 17:19:15 -0300

    Estilização inicial do botão

--- components/Button/componente.jsx
+++ components/Button/componente.jsx
@@ -1,5 +1,7 @@
+import styles from './styles.module.scss'
+
 export default function Button(props) {
     return (
-        <button>{props.children}</button>
+        <button className={styles.button}>{props.children}</button>
     )
 }
