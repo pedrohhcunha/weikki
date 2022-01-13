0f99b11436c7db31708af213bfb31b53d5f56543
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-13 17:09:42 -0300

    Estilização inicial da section inicial

--- components/Inicio/componente.jsx
+++ components/Inicio/componente.jsx
@@ -1,10 +1,15 @@
+import styles from './styles.module.scss'
 import Button from "../Button/componente.jsx"
 
 export default function Inicio(props){
     return(
-        <section>
+        <section className={styles.section}>
             <h4>Qualidade</h4>
-            <h1>Uniformes e EPI's com <strong>inovação e alto padrão em <strong>design</strong></strong>, que proporcionam maior <strong>conforto, durabilidade e segurança</strong>, com a <strong>experiência de quem está há mais de <strong>26 anos no mercado.</strong></strong></h1>
+            <h1>
+                Uniformes e EPI's com <strong>inovação e alto padrão em <strong>design</strong></strong>, <br/>
+                que proporcionam maior <strong className={styles.strongWhite}>conforto, durabilidade <br />
+                e segurança</strong>, com a <strong>experiência de quem está há mais <br/>
+                de <strong>26 anos no mercado.</strong></strong></h1>
             <Button large>Solicitar Orçamento</Button>
         </section>
     )
