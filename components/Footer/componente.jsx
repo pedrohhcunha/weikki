ec5c032cd5009b916fd06bd48c08317cfaaeda4b
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 13:37:51 -0300

    Adiconando estilização para o footer

--- components/Footer/componente.jsx
+++ components/Footer/componente.jsx
@@ -1,23 +1,40 @@
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from './styles.module.scss'
+
 export default function Footer (props){
     return(
-        <footer>
-            <div className="contentArea">
-                <div className="areaLogo"></div>
-                <div className="areaInfos">
-                    <div className="info">
-                        <strong>Endereço:</strong>  Rua Barão do Rio Branco, 1044 E, <br />
-                        Bairro Jardim Itália, Chapecó - SC, CEP: 89.802-101
+        <footer className={styles.footer}>
+            <div className={styles.contentArea}>
+                <div className={styles.areaLogo}></div>
+                <div className={styles.areaInfos}>
+                    <div className={styles.info}>
+                        <span>
+                            <strong>Endereço:</strong>  Rua Barão do Rio Branco, 1044 E, <br />
+                            Bairro Jardim Itália, Chapecó - SC, CEP: 89.802-101
+                        </span>
                     </div>
-                    <div className="info">
-                        <strong>Contato:</strong> <br />
-                        (49) 3322-2593
+                    <div className={styles.info}>
+                        <span>
+                            <strong>Contato:</strong> <br />
+                            <div className={styles.align}>
+                                <FontAwesomeIcon className={styles.icon} icon={faPhone} />
+                                <span>(49) 3322-2593</span>
+                            </div>
+                        </span>
                     </div>
-                    <div className="info">
+                    <div className={styles.info}>
+                        <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
                         <span>weikki@weikki.com.br</span>
                     </div>
                 </div>
+                <div className={styles.areaRedes}>
+                    <div className={styles.rede}></div>
+                    <div className={styles.rede}></div>
+                    <div className={styles.rede}></div>
+                </div>
             </div>
-            <div className="direitosArea">
+            <div className={styles.direitosArea}>
                 Todos os direitos reservados. WEIKKI 2022
             </div>
         </footer>
