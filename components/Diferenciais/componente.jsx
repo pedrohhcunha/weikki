21f722d166e561cf012109274ced6ab05e0455b2
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 15:05:58 -0300

    Adicionando icone padrão para diferenciais

--- components/Diferenciais/componente.jsx
+++ components/Diferenciais/componente.jsx
@@ -1,5 +1,7 @@
 import Button from '../Button/componente.jsx'
 import styles from './styles.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAvianex } from '@fortawesome/free-brands-svg-icons';
 
 export default function Diferenciais(props){
     return(
@@ -8,17 +10,23 @@ export default function Diferenciais(props){
             <h3>Conheça alguns dos nossos diferenciais.</h3>
             <div className={styles.cardsArea}>
                 <div className={styles.diferencial}>
-                    <div className={styles.areaIcon}></div>
+                    <div className={styles.areaIcon}>
+                        <FontAwesomeIcon className={styles.icon} icon={faAvianex} />
+                    </div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
                 <div className={styles.diferencial}>
-                    <div className={styles.areaIcon}></div>
+                    <div className={styles.areaIcon}>
+                        <FontAwesomeIcon className={styles.icon} icon={faAvianex} />
+                    </div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
                 <div className={styles.diferencial}>
-                    <div className={styles.areaIcon}></div>
+                    <div className={styles.areaIcon}>
+                        <FontAwesomeIcon className={styles.icon} icon={faAvianex} />
+                    </div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
