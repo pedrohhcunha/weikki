0d91a7a77ed438b38077d5df2b1405fc900ca011
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 15:21:25 -0300

    Adicionando imagens e icones na section de soluções

--- components/Solucoes/componente.jsx
+++ components/Solucoes/componente.jsx
@@ -2,6 +2,10 @@ import Button from '../Button/componente.jsx'
 import styles from './styles.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAvianex } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image'
+import card1 from './images/card1.jpeg'
+import card2 from './images/card2.jpeg'
+import card3 from './images/card3.jpeg'
 
 export default function Solucoes (props){
     return(
@@ -12,12 +16,12 @@ export default function Solucoes (props){
                 <div className={styles.cardSolucao}>
                     <div className={styles.topArea}>
                         <div className={styles.areaIcon}>
-                        <FontAwesomeIcon className={styles.icon} icon={faAvianex} />
+                            <FontAwesomeIcon className={styles.icon} icon={faAvianex} />
                         </div>
                         <h4>Uniformes profissionais</h4>
                     </div>
                     <div className={styles.areaImagem}>
-
+                        <Image objectFit="cover" src={card1} layout="fill" />
                     </div>
                     <p>
                     Produtos indispensáveis para os setores industrial, alimentício, de saúde e construção civil. <br /> 
@@ -35,7 +39,7 @@ export default function Solucoes (props){
                         <h4>Uniformes profissionais</h4>
                     </div>
                     <div className={styles.areaImagem}>
-
+                        <Image objectFit="cover" src={card2} layout="fill" />
                     </div>
                     <p>
                     Produtos indispensáveis para os setores industrial, alimentício, de saúde e construção civil. <br /> 
@@ -53,7 +57,7 @@ export default function Solucoes (props){
                         <h4>Uniformes profissionais</h4>
                     </div>
                     <div className={styles.areaImagem}>
-
+                        <Image objectFit="cover" src={card3} layout="fill" />
                     </div>
                     <p>
                     Produtos indispensáveis para os setores industrial, alimentício, de saúde e construção civil. <br /> 
