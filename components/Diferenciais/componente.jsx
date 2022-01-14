1c3b395edf62c41cef6459452ca8068aecefcfe4
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 09:38:48 -0300

    Estilizando nossos diferenciais

--- components/Diferenciais/componente.jsx
+++ components/Diferenciais/componente.jsx
@@ -1,34 +1,31 @@
 import Button from '../Button/componente.jsx'
+import styles from './styles.module.scss';
 
 export default function Diferenciais(props){
     return(
-        <section>
+        <section className={styles.section}>
             <h2>Nossos Diferenciais</h2>
             <h3>Conheça alguns dos nossos diferenciais.</h3>
-            <div className="cardsArea">
-                <div className="diferencial">
-                    <div className="areaIcon">
-
-                    </div>
+            <div className={styles.cardsArea}>
+                <div className={styles.diferencial}>
+                    <div className={styles.areaIcon}></div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
-                <div className="diferencial">
-                    <div className="areaIcon">
-
-                    </div>
+                <div className={styles.diferencial}>
+                    <div className={styles.areaIcon}></div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
-                <div className="diferencial">
-                    <div className="areaIcon">
-
-                    </div>
+                <div className={styles.diferencial}>
+                    <div className={styles.areaIcon}></div>
                     <h4>Praticidade e Agilidade</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum quas consequatur repellendus et quibusdam voluptate cupiditate. Ipsam minima labore deserunt iste quisquam accusantium, quam dolorem consequatur, quod, laborum mollitia.</p>
                 </div>
             </div>
-            <Button>Falar com especialista</Button>
+            <div className={styles.areaButton}>
+                <Button>Falar com especialista</Button>
+            </div>
         </section>
     )
 }
