649bfacf1b638832259c3b264c09b6abd6a314cd
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 11:23:38 -0300

    Seciton final

--- components/Button/componente.jsx
+++ components/Button/componente.jsx
@@ -2,6 +2,6 @@ import styles from './styles.module.scss'
 
 export default function Button(props) {
     return (
-        <button className={`${styles.button} ${props.large ? styles.large : ''} `}>{props.children}</button>
+        <button className={`${styles.button} ${props.large ? styles.large : ''} ${props.white ? styles.white : ''}`}>{props.children}</button>
     )
 }
--- components/Final/componente.jsx
+++ components/Final/componente.jsx
@@ -1,11 +1,15 @@
+import styles from './styles.module.scss'
+import Button from '../Button/componente.jsx'
+
 export default function Final(props){
     return(
-        <section>
-            <div className="areaLogo"></div>
-            <div className="areaContent">
+        <section className={styles.section}>
+            <div className={styles.areaLogo}></div>
+            <div className={styles.areaContent}>
                 <h2>+Durabilidade</h2>
                 <p>Estamos prontos para atender a sua necessidade com durabilidade, segurança e conforto.
                 Nosso time de especialistas está aqui para encontrar <strong>a melhor solução para a sua demanda</strong>.</p>
+                <Button white>Falar com especialista</Button>
             </div>
         </section>
     )
