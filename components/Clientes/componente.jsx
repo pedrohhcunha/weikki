eb82c46d5f050174fa1172352bcc110b1b84dde6
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-14 10:20:19 -0300

    Estilização da section de quem confia na gente

--- components/Clientes/componente.jsx
+++ components/Clientes/componente.jsx
@@ -1,27 +1,32 @@
+import styles from './styles.module.scss';
+
 export default function Clientes (props){
     return(
-        <section>
+        <section className={styles.section}>
             <h2>Quem confia na gente</h2>
             <h3>Mais de 400 empresas já confiaram na Weikki.</h3>
-            <div className="clientes">
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
-                <div className="cliente">Empresa</div>
+            <div className={styles.clientes}>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
+                <div className={styles.cliente}>Empresa</div>
             </div>
         </section>
     )
