8366958359fd532b7ed3ec3568ea75fa85f13abf
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-17 11:05:33 -0300

    adicionando imagem padrão

--- components/Sobre/componente.jsx
+++ components/Sobre/componente.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './styles.module.scss'
+import img1 from './images/img1.jpg'
 
 export default function Sobre(props) {
     return(
@@ -7,7 +8,7 @@ export default function Sobre(props) {
             <h3>Indústria de Uniformes e EPI&apos;s com alto padrão de qualidade e atendimento consultivo.</h3>
             <h4>Desde 1996, nosso propósito é satisfazer nossos clientes e atender às suas necessidades com ética, confiança e respeito.</h4>
            <div className={styles.areaCarrossel}>
-
+            <Image priority objectFit="cover" src={img1} layout="fill" />
            </div>
         </section>
     )
