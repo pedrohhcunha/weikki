dd0d816ffee0e964cfa35dff90fc5831ac45d571
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-17 15:27:19 -0300

    Adicionando as fotos da wk

--- components/Sobre/componente.jsx
+++ components/Sobre/componente.jsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import styles from './styles.module.scss'
-import img1 from './images/img1.jpg'
-import img2 from './images/banner.jpeg'
+import img1 from './images/1.jpg'
+import img2 from './images/2.jpg'
+import img3 from './images/3.jpg'
+import img4 from './images/4.jpg'
+import img5 from './images/5.jpg'
+import img6 from './images/6.jpg'
+import img7 from './images/7.jpg'
 
 import { useState, useEffect } from 'react';
 
@@ -12,6 +17,21 @@ export default function Sobre(props) {
         },
         {
             src: img2
+        },
+        {
+            src: img3
+        },
+        {
+            src: img4
+        },
+        {
+            src: img5
+        },
+        {
+            src: img6
+        },
+        {
+            src: img7
         }
     ]);
 
