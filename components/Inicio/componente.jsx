6540d9efb509bb1fd79cecf71371baeffc864cc7
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-17 10:22:30 -0300

    Removendo erro nas aspas

--- components/Inicio/componente.jsx
+++ components/Inicio/componente.jsx
@@ -6,7 +6,7 @@ export default function Inicio(props){
         <section className={styles.section}>
             <h4>Qualidade</h4>
             <h1>
-                Uniformes e EPI's com <strong>inovação e alto padrão em <strong>design</strong></strong>, <br/>
+                Uniformes e EPI&apos;s com <strong>inovação e alto padrão em <strong>design</strong></strong>, <br/>
                 que proporcionam maior <strong className={styles.strongWhite}>conforto, durabilidade <br />
                 e segurança</strong>, com a <strong>experiência de quem está há mais <br/>
                 de <strong>26 anos no mercado.</strong></strong></h1>
