202becbf0eb96723de7d0a6e52565b46771ca2a7
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-18 16:48:10 -0300

    Adicionando animação no titulo

--- components/Final/componente.jsx
+++ components/Final/componente.jsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss'
 import Button from '../Button/componente.jsx'
 import logo from './images/logo_vazado.png'
 import Image from 'next/image'
+import Typewriter from "typewriter-effect";
 
 export default function Final(props){
     return(
@@ -10,7 +11,19 @@ export default function Final(props){
                 <Image src={logo} className={styles.logo} layout='responsive' />
             </div>
             <div className={styles.areaContent}>
-                <h2>+DURABILIDADE</h2>
+                <h2><Typewriter options={{loop: true}} onInit={typewriter => {
+                    
+                    typewriter
+                    .typeString("+Qualidade")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("+Praticidade")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("+Bem-estar")
+                    .pauseFor(1000)
+                    .start()
+                }}/></h2>
                 <p>Estamos prontos para atender a sua necessidade com durabilidade, segurança e conforto. <br />
                 Nosso time de especialistas está aqui para encontrar <strong>a melhor solução para a sua demanda</strong>.</p>
                 <Button white>Falar com especialista</Button>
--- components/Inicio/componente.jsx
+++ components/Inicio/componente.jsx
@@ -1,10 +1,25 @@
+import Typewriter from "typewriter-effect";
 import styles from './styles.module.scss'
 import Button from "../Button/componente.jsx"
 
 export default function Inicio(props){
     return(
         <section className={styles.section}>
-            <h4>Qualidade</h4>
+            <h4>
+                <Typewriter options={{loop: true}} onInit={typewriter => {
+                
+                    typewriter
+                    .typeString("+Qualidade")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("+Praticidade")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("+Bem-estar")
+                    .pauseFor(1000)
+                    .start()
+                }}/>
+            </h4>
             <h1>
                 Uniformes e EPI&apos;s com <strong>inovação e alto padrão em <strong>design</strong></strong>,
                 que proporcionam maior <strong className={styles.strongWhite}>conforto, durabilidade
