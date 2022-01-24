f75bf96cbedbbb7b1b794547461c4463a8959e1b
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-24 09:25:03 -0300

    Refatorando a section final do componenteg

--- components/Final/componente.jsx
+++ components/Final/componente.jsx
@@ -1,10 +1,18 @@
+//Componente para are final do sites - antes do footer
+
+//Importando módulo para estilização do componente
 import styles from './styles.module.scss'
+
+//Importando componentes necessários
 import Button from '../Button/componente.jsx'
 import logo from './images/logo_vazado.png'
 import Image from 'next/image'
 import Typewriter from "typewriter-effect";
 
+//Definindo e exportando o componente
 export default function Final(props){
+
+    //Retorando o JSX do componente
     return(
         <section className={styles.section}>
             <div className={styles.areaLogo}>
