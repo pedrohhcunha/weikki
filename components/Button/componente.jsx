f6f6e8c18ecd203df7e7840e1831e14062865bd6
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-24 09:33:41 -0300

    Refatorando o componente de botão

--- components/Button/componente.jsx
+++ components/Button/componente.jsx
@@ -1,6 +1,13 @@
+//Componente de botão para ser usado em qualquer um dos outros componentes
+// white BOOLEAN - Define se o botão será com o fundo branco ou não
+// large BOOLEAN - Define se o botão será maior que os demais ou não
+
+//Importando módulo para a estilização do componente
 import styles from './styles.module.scss'
 
+//Definindo e exportando o componente
 export default function Button(props) {
+    //Retorando o JSX do componente
     return (
         <button onClick={props.onClick} className={`${styles.button} ${props.large ? styles.large : ''} ${props.white ? styles.white : ''}`}>{props.children}</button>
     )
