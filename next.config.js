9d9895b23bf32572d9fab9fe177906a88117ef7c
Carlos Giongo <carlosgiongoo@gmail.com>
2022-03-09 16:29:27 -0300

    Ajuste

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  domains: ['localhost', 'api.onfinity.com.br', 'teste-api.onfinity.com.br']
+  domains: ['localhost', 'api.onfinity.com.br', 'teste-api.onfinity.com.br', '192.168.30.46'],
+  images: {
+    domains: ['localhost', 'septi.life', '192.168.30.46', 'api.onfinity.com.br', 'teste-api.onfinity.com.br']
+  },
 }
