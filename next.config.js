3291e46c4b6db3ff7b20d5b8c1fffe2bf80f9631
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-03-10 08:45:37 -0300

    Change the domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  domains: ['localhost', 'api.onfinity.com.br', 'teste-api.onfinity.com.br', '192.168.30.92'],
+  domains: ['localhost', 'api.onfinity.com', 'teste-api.onfinity.com', '192.168.30.92'],
   images: {
-    domains: ['localhost', 'septi.life', '192.168.30.92', 'api.onfinity.com.br', 'teste-api.onfinity.com.br']
+    domains: ['localhost', 'septi.life', '192.168.30.92', 'api.onfinity.com', 'teste-api.onfinity.com']
   },
 }
