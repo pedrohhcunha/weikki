bd26f86ba5d3df76afb5fea733cde5e3de3219d9
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-03-10 09:17:14 -0300

    domains update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   domains: ['localhost', 'api.onfinity.com.br', 'teste-api.onfinity.com.br', '192.168.30.92'],
   images: {
-    domains: ['localhost', 'septi.life', '192.168.30.92', 'api.onfinity.com.br', 'teste-api.onfinity.com.br']
+    domains: ['localhost', 'septi.life', '192.168.30.92', 'https://api.onfinity.com.br', 'https://teste-api.onfinity.com.br']
   },
 }
