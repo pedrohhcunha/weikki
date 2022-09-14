1560ca2bb93c93095c9875b6ab8d11f3e252c06c
Vinicios Azambuja <70420888+viniciosazambuja@users.noreply.github.com>
2022-09-14 11:18:05 -0300

    Add google tag manager

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,6 +41,14 @@ export default function Home() {
         <meta name="facebook-domain-verification" content="2tvzs8l0f00vthterv5hopcb37jyza" />
         
         <script dangerouslySetInnerHTML={{__html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NF4CXJP');`}}>
+        </script>
+
+        <script dangerouslySetInnerHTML={{__html: `
              (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:2791333,hjsv:6};
